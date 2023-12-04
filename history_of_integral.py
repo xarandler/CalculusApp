26cbca1dd02cf1a3e251e12ae6ed7ec10e826689
Anders Randler <xarax@me.com>
2023-12-04 08:40:41 +0100

    First version
    This is the first version uploaded

---
+++ history_of_integral.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def show_history_of_integral():
+    st.title("History of Integral")
+    st.write("""
+        Here you can add detailed content about the history of integral calculus.
+        For example:
+
+        - The integral, from Latin 'integrare' meaning 'to make whole', ...
+        - The concept of an integral can be traced back to the Ancient Greeks, ...
+        - The formal study of calculus started in Europe during the 17th century, ...
+    """)
